0def9b2d39923ddbe43b793d2dc99bc51e774293
Siddharth <Siddharth@MSI>
2021-09-08 18:27:02 +0530

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System.Web.Mvc;
+using WebApplication1.Models;
+using CalculatorLib;
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Calculator()
+        {
+           
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calculator(Calc model, string command)
+        {
+          Calculator clac = new Calculator();
+            if (command == "add")
+            {
+                
+                model.Result = clac.Add( model.A ,model.B);
+            }
+            if (command == "sub")
+            {
+                model.Result = clac.Subtract(model.A, model.B);
+            }
+            if (command == "mul")
+            {
+                model.Result = clac.Multiply(model.A, model.B);
+            }
+            if (command == "div")
+            {
+                model.Result = clac.Division(model.A, model.B);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication1/Models/Calc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Calc
+    {
+     
+         public double A
+        {
+            get;
+            set;
+        }
+        public double B
+        {
+            get;
+            set;
+        }
+        public double Result
+        {
+            get;
+            set;
+        }
+    }
+}
